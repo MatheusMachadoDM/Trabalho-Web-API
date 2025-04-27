c2d69ca8ceece42980be6e01fe34a3932e15ddd0
Lucca Anderle <lucca.anderle1512@gmail.com>
2025-04-27 01:26:11 -0300

    Feitos os métodos Get para achar hospedes e reservas

--- HotelAPI/Program.cs
+++ HotelAPI/Program.cs
@@ -21,27 +21,8 @@ builder.Services.AddDbContext<AppDataContext>();
  }
  
  app.UseHttpsRedirection();
- 
- var summaries = new[]
- {
-     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
- };
- 
- app.MapGet("/weatherforecast", () =>
- {
-     var forecast =  Enumerable.Range(1, 5).Select(index =>
-         new WeatherForecast
-         (
-             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-             Random.Shared.Next(-20, 55),
-             summaries[Random.Shared.Next(summaries.Length)]
-         ))
-         .ToArray();
-     return forecast;
- })
- .WithName("GetWeatherForecast")
- .WithOpenApi();
- 
+
+// Cadastro de Reserva já com o hospede junto 
 app.MapPost("/HotelAPI/reserva", ([FromBody] Reserva reserva, [FromServices] AppDataContext context) => {
     
     Hospede? hospede = context.Hospedes.FirstOrDefault(h => h.CPF == reserva.Hospede.CPF); // procurando o hospede por cpf, a funcao retorna o primeiro registro que der match ou retorna null caso nao encontre
@@ -56,36 +37,56 @@ app.MapPost("/HotelAPI/reserva", ([FromBody] Reserva reserva, [FromServices] App
         return Results.Created("", reserva);
 });
 
+// Lista todas as reservas
+app.MapGet("/HotelAPI/reserva", ([FromServices] AppDataContext context) => {
+    if(context.Reservas.Any()){            // Necessário o include, pois o campo Hospede, aparecia como null.
+        return Results.Ok(context.Reservas.Include(r => r.Hospede).ToList()); //Isso ocorre, pois aparentemente o ef core não carrega dados relacionados automaticamente
+    }
+
+    return Results.NotFound();
+});
+
+/*// Buscar uma reserva específica por Id
+app.MapGet("/HotelAPI/reserva{ReservaId}", ([FromRoute] int ReservaId, [FromServices] AppDataContext context) => {
+    Reserva? reserva = context.Reservas.Include(r => r.Hospede).FirstOrDefault(r => r.ReservaId == ReservaId); // Precisei utilizar o FirstOrDefault, pois o Find aparentemente
+                                                                                                               // não permite utilizar o Include      
+
+    if(reserva != null){
+        return Results.Ok(reserva);
+    }
+
+    return Results.NotFound();
+});*/
+
+// Lista todas as reservas atribuidas a um cpf específico
+app.MapGet("/HotelAPI/reserva/hospede{CPF}", ([FromRoute] long CPF, [FromServices] AppDataContext context) => {
+    var reservasHospede = context.Reservas.Include(r => r.Hospede).Where(r => r.Hospede.CPF == CPF).ToList();
+
+    if(reservasHospede.Any()){
+        return Results.Ok(reservasHospede);
+    }
+
+    return Results.NotFound();
+});
+
+// Lista todos os clientes 
+app.MapGet("/HotelAPI/hospede", ([FromServices] AppDataContext context) => {
+    if(context.Hospedes.Any()){
+        return Results.Ok(context.Hospedes.ToList());
+    }
+
+    return Results.NotFound();
+});
+
+// Buscar um Hospede específico por CPF
+app.MapGet("/HotelAPI/hospede{CPF}", ([FromRoute] long CPF, [FromServices] AppDataContext context) => {
+    Hospede? hospede = context.Hospedes.FirstOrDefault(h => h.CPF == CPF);
+
+    if(hospede != null){
+        return Results.Ok(hospede);
+    }
+
+    return Results.NotFound();
+});
+
  app.Run();
- 
- record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
- {
-     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
- }
-/*using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();*/
-
