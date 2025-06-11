5122ccd0a1caf0dd5196130d2fe78a855c3b394a
MatheusMachadoDM <165315314+MatheusMachadoDM@users.noreply.github.com>
2025-06-11 20:43:49 -0300

    Update index.tsx

--- hotel-frontend/src/index.tsx
+++ hotel-frontend/src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
+//Importa o render do app
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//Conecta a div root com o react
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+//Renderiza o app dentro do root
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
